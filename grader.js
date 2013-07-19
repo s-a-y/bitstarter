92ac51e0afa6a10ec26d893f6fdd4179949d4828
Sergey <sergey.a.yusupov@gmail.com>
2013-07-19 17:19:23 +0000

    Improved index.html using Bootstrap and other stuff

--- grader.js
+++ grader.js
@@ -24,8 +24,22 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var restler = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+
+var downloadFile = function(url) {
+    if (!url) return null;
+
+    restler.get(url.toString()).on('complete', function(result, response) {
+      if (result instanceof Error) {
+          console.error('Error: ' + util.format(response.message));
+      } else {
+          fs.writeFileSync('downloaded.html', result);
+          return 'downloaded.html';
+      }
+    });
+}
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -65,8 +79,10 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'Url of index.html', clone(downloadFile), null)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+
+    var checkJson = checkHtmlFile(program.url || program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
